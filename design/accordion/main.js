f9c7996cdaff015c62aaad2a03f8924411b2438e
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-20 18:08:36 +0300

    Rearange the files

--- design/accordion/main.js
+++ design/accordion/main.js
@@ -2,7 +2,7 @@ const element = document.querySelectorAll('.item');
 const button = document.querySelector('.icon');
 
 element.forEach((item) => {
-    item.addEventListener('click', () => {
+    item.addEventListener('click', (e) => {
         item.classList.toggle('open');
     });
     }
