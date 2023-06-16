99031f33dbd9f03385a6d42266ffc060c5c19799
IBUNHABIBU <ibunhabib@gmail.com>
2023-06-16 09:06:45 +0300

    Add javascript

--- omnifood/js/script.js
+++ omnifood/js/script.js
@@ -3,3 +3,5 @@ const menuBtn = document.querySelector('.btn-mobile-nav');
 menuBtn.addEventListener('click', () => {
     document.querySelector('.header').classList.toggle('nav-open');
 });
+
+const allLinks = document.querySelectorAll('a:link');
