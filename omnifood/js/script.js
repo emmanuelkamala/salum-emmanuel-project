282372b91fd7ce811093e97fdea02fa285b59db7
IBUNHABIBU <ibunhabib@gmail.com>
2023-06-17 08:09:15 +0300

    Add smooth scroll

--- omnifood/js/script.js
+++ omnifood/js/script.js
@@ -5,3 +5,17 @@ menuBtn.addEventListener('click', () => {
 });
 
 const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const href = link.getAttribute('href');
+
+        if(href === '#') {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
